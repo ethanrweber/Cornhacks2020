f3826176ab35fda98fa2a01d9fceb80a6bebced1
Ethan Weber <ethanrweber@gmail.com>
2020-01-18 15:35:22 -0600

    Add Google Maps

---
+++ CornhacksProject/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CornhacksProject.Models
+{
+    public class Constants
+    {
+        public static readonly string googleMapsApiKey = "AIzaSyDI_9X0MDlx3SZ-g0Vwp_l6UEfzVKF1HGo";
+    }
+}

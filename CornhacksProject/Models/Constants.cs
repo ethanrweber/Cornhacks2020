cc271f2c645056cfc5fd73348563b0a25e782d1f
Ethan Weber <ethanrweber@gmail.com>
2020-01-18 17:23:46 -0600

    Figured out how to request json data

--- CornhacksProject/Models/Constants.cs
+++ CornhacksProject/Models/Constants.cs
@@ -8,5 +8,6 @@ namespace CornhacksProject.Models
     public class Constants
     {
         public static readonly string googleMapsApiKey = "AIzaSyDI_9X0MDlx3SZ-g0Vwp_l6UEfzVKF1HGo";
+        public static readonly string airVisualApiKey = "fb354654-e77f-4fc7-8fb0-5d7e30cfac70";
     }
 }

a2be2cb1473833edaf254297fb62553e11a903fe
Ethan Weber <ethanrweber@gmail.com>
2020-01-19 00:14:40 -0600

    Update map and Constants
    - map now expands to view all search items
    - added Application Name to constants

--- CornhacksProject/Models/Constants.cs
+++ CornhacksProject/Models/Constants.cs
@@ -9,7 +9,7 @@ namespace CornhacksProject.Models
     {
         public const string ApplicationName = "CityScore";
 
-        public static readonly string googleMapsApiKey = "AIzaSyDI_9X0MDlx3SZ-g0Vwp_l6UEfzVKF1HGo";
+        public static readonly string googleMapsApiKey = "AIzaSyD_0RvglWdwS3ICkEjP9BzlmeherfKZv6Q";
         public static readonly string airVisualApiKey = "fb354654-e77f-4fc7-8fb0-5d7e30cfac70";
     }
 }

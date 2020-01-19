7855d1347e81a915a865f0b04800d3bb49c24cea
Ethan Weber <ethanrweber@gmail.com>
2020-01-19 04:56:24 -0600

    Project works 99% but results aren't very interesting

--- CornhacksProject/Controllers/HomeController.cs
+++ CornhacksProject/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 using CornhacksProject.Models;
 using Newtonsoft.Json;
@@ -20,7 +19,7 @@ namespace CornhacksProject.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-            var z = SustainabilityScore("Lincoln", "Nebraska", "USA");
+
             return View();
         }
 
@@ -29,7 +28,7 @@ namespace CornhacksProject.Controllers
             return View();
         }
 
-        public async Task<AirVisualResult> GetAirVisualResultAsync(string city, string state, string country)
+        public async Task<AirVisualResult> GetAirVisualResultAsync(string city, string state)
         {
             //testing
             if(string.IsNullOrWhiteSpace(city))
@@ -37,15 +36,13 @@ namespace CornhacksProject.Controllers
             city = "city=" + city;
             if (!string.IsNullOrWhiteSpace(state))
                 state = "&state=" + state;
-            if (!string.IsNullOrWhiteSpace(country))
-                country = "&country=" + country;
 
             using (var client = new HttpClient())
             {
-                string addy = $"https://api.airvisual.com/v2/city?{city}{state}{country}&key={Constants.airVisualApiKey}";
-                var response = client.GetStringAsync(new Uri(addy)).Result;
-                var x = JsonConvert.DeserializeObject<AirVisualResult>(response);
-                return x;
+                string address = $"https://api.airvisual.com/v2/city?{city}{state}&country=USA&key={Constants.airVisualApiKey}";
+                string response = client.GetStringAsync(new Uri(address)).Result;
+                var AVresult = JsonConvert.DeserializeObject<AirVisualResult>(response);
+                return AVresult;
             }
         }
 
@@ -53,29 +50,14 @@ namespace CornhacksProject.Controllers
         {
             using (var client = new HttpClient())
             {
-                string addy = $"https://api.openchargemap.io/v3/poi/?output=json&compact=true&verbose=false&countrycode=US&latitude={latitude}&longitude={longitude}&distance={distance}&distanceunit={distanceUnit}";
-                var response = client.GetStringAsync(new Uri(addy)).Result;
-                var x = JsonConvert.DeserializeObject<List<OpenChargeResult>>(response);
-
-                return x;
+                string address = $"https://api.openchargemap.io/v3/poi/?output=json&compact=true&verbose=false&countrycode=US&latitude={latitude}&longitude={longitude}&distance={distance}&distanceunit={distanceUnit}";
+                string response = client.GetStringAsync(new Uri(address)).Result;
+                var OCresult = JsonConvert.DeserializeObject<List<OpenChargeResult>>(response);
+                return OCresult;
             }
         }
 
-        public int GetNumEVStations(List<OpenChargeResult> list, string city)
-        {
-            int numStations = 0;
-            foreach (var v in list)
-            {
-                if (v.AddressInfo.Town == city)
-                {
-                    numStations++;
-                }
-            }
-
-            return numStations;
-        }
-
-        public async Task<EnergyConsumption> GetEnergyConsumptionsResultsAsync(string city, string state, string country)
+        public async Task<int> GetEnergyConsumptionsResultsAsync(string city, string state)
         {
             //testing
             if (string.IsNullOrWhiteSpace(city))
@@ -85,81 +67,90 @@ namespace CornhacksProject.Controllers
             {
                 string stateAbrev = "";
                 if (Constants.states.ContainsKey(state.ToLower()))
-                {
                     stateAbrev = Constants.states[state.ToLower()];
-                }
+
                 state = "&state_abbr=" + stateAbrev;
             }
-            if (!string.IsNullOrWhiteSpace(country))
-                country = "&country=" + country;
 
             using (var client = new HttpClient())
             {
-                string addy = $"https://developer.nrel.gov/api/cleap/v1/energy_cohort_data?{city}{state}&api_key={Constants.energyConsumptionApiKey}";
-                var response = client.GetStringAsync(new Uri(addy)).Result;
+                string address = $"https://developer.nrel.gov/api/cleap/v1/energy_cohort_data?{city}{state}&api_key={Constants.energyConsumptionApiKey}";
+                string response = client.GetStringAsync(new Uri(address)).Result;
+                // non-standard result - include location name, can't standardize class the same way as others
                 string s = response.Substring(response.IndexOf("\"table\""));
                 s = "{" + s.Substring(0, s.Length-3);
-                var x = JsonConvert.DeserializeObject<EnergyConsumption>(s);
-                return x;
+                var ECResultTable = JsonConvert.DeserializeObject<EnergyConsumption>(s).table;
+                return ECResultTable.residential_electric_use.avg;
             }
         }
 
-        public async Task<CityPopulation> GetPopulationAsync(string city, string state, string country)
+        public async Task<CityPopulation> GetPopulationAsync(string city, string state)
         {
-            //testing
-            string query = "";
             if (string.IsNullOrWhiteSpace(city))
                 throw new Exception("city is a required field");
-            query = "q=" + city;
+            string query = "q=" + city;
             if (!string.IsNullOrWhiteSpace(state))
             {
                 string stateAbrev = "";
                 if (Constants.states.ContainsKey(state.ToLower()))
-                {
                     stateAbrev = Constants.states[state.ToLower()];
-                }
+                
                 query = query + " " + stateAbrev + "&";
             }
-            if (!string.IsNullOrWhiteSpace(country))
-                country = "&country=" + country;
 
             using (var client = new HttpClient())
             {
                 string addy = $"https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop&{query}rows=1&sort=population&facet=country&refine.country=us";
                 var response = client.GetStringAsync(new Uri(addy)).Result;
-                var x = JsonConvert.DeserializeObject<CityPopulation>(response);
-                return x;
+                var PopResult = JsonConvert.DeserializeObject<CityPopulation>(response);
+                return PopResult;
             }
         }
 
-        public double SustainabilityScore(string city, string state, string country)
+        public double SustainabilityScore(string city, string state)
         {
-            var p = GetPopulationAsync(city, state, country).Result;
+            // convert 2 letter state to full name for some requests
+            // ex: given NE, select dictionary key for the first entry with value NE (key is full name)
+            if (state.Length == 2)
+                state = Constants.states.FirstOrDefault(s => s.Value == state).Key;
+
+            // population data
+            CityPopulation p = GetPopulationAsync(city, state).Result;
+            int? population = p.records.FirstOrDefault()?.fields.population;
+            if(population == null)
+                throw new Exception("Population data error");
+
+            // use coords from population data to find ev chargers
+            // ev charger score (bonus points if > avg)
             List<double> coordinates = p.records[0].geometry.coordinates;
-            var z = GetOpenChargeResultAsync(coordinates[1], coordinates[0]).Result;
-            int numEVStations = GetNumEVStations(z, city);
+            var OCResultList = GetOpenChargeResultAsync(coordinates[1], coordinates[0]).Result;
+            int numEVStations = OCResultList.Count(ocr => ocr.AddressInfo.Town == city);
+            double EVStationScoreBonus = (double) numEVStations / population.Value;
 
-            int population = p.records.FirstOrDefault().fields.population;
+            // energy consumption score
+            int avgResidentialEnergyUse = GetEnergyConsumptionsResultsAsync(city, state).Result;
+            double energyScore = (double) avgResidentialEnergyUse / population.Value;
 
-
-            var energyStats = GetEnergyConsumptionsResultsAsync(city, state, country).Result;
-            int industrialGasUse = energyStats.table.industrial_gas_use.avg;
-            int industrialElectricUse = energyStats.table.industrial_electric_use.avg;
-            int commercialGasUse = energyStats.table.commercial_gas_use.avg;
-            int commercialElectricUse = energyStats.table.commercial_electric_use.avg;
-            int residentialGasUse = energyStats.table.residential_gas_use.avg;
-            int residentialElectricUse = energyStats.table.residential_electric_use.avg;
-            int cityGasUse = energyStats.table.city_fuel_use_gas.avg;
-
-            var airResult = GetAirVisualResultAsync(city, state, country).Result;
-            //the lower, the better:
+            // air quality value
+            var airResult = GetAirVisualResultAsync(city, state).Result;
             int airQuality = airResult.data.current.pollution.aqius;
 
-            double energyScore = (double) (industrialElectricUse + industrialGasUse + commercialGasUse + commercialElectricUse + residentialElectricUse + residentialGasUse + cityGasUse) / population;
-            double airScore = (double) airQuality / population;
+            // air quality score based on us official air quality index rankings 
+            int airScore;
+            if (airQuality <= 50) airScore = 6;
+            else if (airQuality <= 100) airScore = 5;
+            else if (airQuality <= 200) airScore = 4;
+            else if (airQuality <= 300) airScore = 3;
+            else if (airQuality <= 400) airScore = 2;
+            else if (airQuality <= 500) airScore = 1;
+            else throw new Exception("api error: value not in expected range");
 
-            //TODO: figure out a better system for scores
-            var finalScore = energyScore * 0.25 + airQuality * 0.75;
+            int finalScore = 10;
+            if (airScore < 6) finalScore -= (7 - airScore) / 2; // deductions for poor air quality (max of 3 removed)
+
+
+            if (EVStationScoreBonus > .0002) finalScore++; // bonus point for ev chargers: approximate avg # evc / population = .2 / 1000 = .0002
+            
 
             return finalScore;
         }
